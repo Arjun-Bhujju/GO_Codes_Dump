f5f8c79443fde816441ee7b83c95f252a313df1b
Arjun Bhujiawala <arjunbhujiawala@gmail.com>
2025-04-10 19:30:28 +0530

    added a swapped flag

--- BubbleSort.go
+++ BubbleSort.go
@@ -2,19 +2,25 @@ package main
 
 import "fmt"
 
-func BubbleSort(arr []int) []int {
-    n := len(arr)
-    for i := 0; i < n-1; i++ {
-        for j := 0; j < n-i-1; j++ {
-            if arr[j] > arr[j+1] {
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-            }
-        }
-    }
-    return arr
+func bubbleSort(arr []int) []int {
+	n := len(arr)
+	for i := 0; i < n-1; i++ {
+		swapped := false
+		for j := 0; j < n-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+				swapped = true
+			}
+		}
+		if !swapped {
+			break // Array is already sorted
+		}
+	}
+	return arr
 }
 
 func main() {
-    numbers := []int{64, 34, 25, 12, 22, 11, 90}
-    fmt.Println("Sorted array:", BubbleSort(numbers))
+	numbers := []int{64, 34, 25, 12, 22, 11, 90}
+	sorted := bubbleSort(numbers)
+	fmt.Println("Sorted array:", sorted)
 }
